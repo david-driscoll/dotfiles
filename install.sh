840a18a2aa4f9dab84f2f6be39c7f797b60ee046
David Driscoll <david.driscoll@gmail.com>
2020-05-24 00:57:09 -0400

    Updated dotfiles

---
+++ install.sh
@@ -0,0 +1,28 @@
+# dotfiles installer
+sudo apt-get update
+
+curl -fsSL https://starship.rs/install.sh | bash
+sudo apt-get install -y python3 python3-pip
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
+sudo pip install thefuck
+
+cp -f ./.bashrc ~/.bashrc
+chmod 644 ~/.bashrc
+cp -f ./.inputrc ~/.inputrc
+chmod 644 ~/.inputrc
+cp -f ./.bash_aliases ~/.bash_aliases
+chmod 644 ~/.bash_aliases
+
+cp -f ./thefuck/ ~/.config/thefuck/
+cp -f ./powershell/ ~/.config/powershell/
+find ~/.config/thefuck/ -type f -print0 | xargs -0 chmod 644
+'. "~/dotfiles/profile.ps1"' >~/.config/powershell/Microsoft.PowerShell_profile.ps1
+
+git config --global core.eol lf
+git config --global core.autocrlf true
+git config --global github.user david-driscoll
+git config --global user.name "David Driscoll"
+git config --global user.email "david.driscoll@gmail.com"
+git config --global core.editor "vi"
+git config --global alias.amend "commit --amend --reuse-message=HEAD"
