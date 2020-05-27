d0dc1385e600a78fa72a3902bff59b1cb71396ea
David Driscoll <david.driscoll@gmail.com>
2020-05-27 11:21:42 -0400

    Updated init config to include window title function

--- profile.pwsh/init.ps1
+++ profile.pwsh/init.ps1
@@ -16,5 +16,16 @@ $ENV:STARSHIP_CONFIG = Join-Path $PSScriptRoot '../starship.toml'
 $ENV:USER = $ENV:USERNAME
 
 iex (&starship init powershell)
+$ExecutionContext.InvokeCommand.LocationChangedAction = {
+
+    $env:PWD = $PWD
+    $current_directory = (Convert-Path $PWD)
+
+    $title = @(&"C:\ProgramData\chocolatey\lib\starship\tools\starship.exe" module directory "--path=$current_directory")
+    $title += @(&"C:\ProgramData\chocolatey\lib\starship\tools\starship.exe" module git_branch "--path=$current_directory")
+
+    $host.UI.RawUI.WindowTitle = $title -replace '(\[\d(?:;\d+)?m)', ''
+}
+
 $env:PYTHONIOENCODING = "utf-8"
 iex "$(thefuck --alias)"
