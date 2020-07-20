0bf3c742ed34cb7900aa15e12b55c509608c080e
David Driscoll <david.driscoll@gmail.com>
2020-07-19 20:21:18 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -4,29 +4,39 @@ sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security m
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 
-curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
+curl -fsSL https://starship.rs/install.sh | sudo bash -s -- --yes
 sudo apt-get install -y python3 python3-pip powershell
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
 sudo pip install thefuck
 
-cp -f ./.bashrc ~/.bashrc
+touch ~/.bashrc > /dev/null 2>&1
+cp -f ~/dotfiles/.bashrc ~/.bashrc
 chmod 644 ~/.bashrc
-cp -f ./.inputrc ~/.inputrc
+
+touch ~/.inputrc > /dev/null 2>&1
+cp -f ~/dotfiles/.inputrc ~/.inputrc
 chmod 644 ~/.inputrc
-cp -f ./.bash_aliases ~/.bash_aliases
+
+touch ~/.bash_aliases > /dev/null 2>&1
+cp -f ~/dotfiles/.bash_aliases ~/.bash_aliases
 chmod 644 ~/.bash_aliases
 
-mkdir ~/.config/
-mkdir ~/.config/thefuck/
+mkdir ~/.config/ > /dev/null 2>&1
+mkdir ~/.config/thefuck/ > /dev/null 2>&1
 cp -rf ~/dotfiles/thefuck/ ~/.config/
-mkdir ~/.config/powershell/
+mkdir ~/.config/powershell/ > /dev/null 2>&1
 cp -rf ~/dotfiles/powershell/ ~/.config/
 find ~/.config/thefuck/ -type f -print0 | xargs -0 chmod 644
 echo '. "~/dotfiles/profile.ps1"' >~/.config/powershell/Microsoft.PowerShell_profile.ps1
 
+if [ $WT_SESSION ]; then
+    WINDOWS_USER=$(/mnt/c/Windows/System32/cmd.exe /c 'echo %USERNAME%' | sed -e 's/\r//g') > /dev/null 2>&1
+    cp -rf /mnt/c/Users/$WINDOWS_USER/.ssh/ ~/.ssh/
+fi
+
 git config --global core.eol lf
-git config --global core.autocrlf true
+git config --global core.autocrlf false
 git config --global github.user david-driscoll
 git config --global user.name "David Driscoll"
 git config --global user.email "david.driscoll@gmail.com"
