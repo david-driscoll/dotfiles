1bd1c8e766296b519c3a8c8fa393ee4c4e421335
David Driscoll <david.driscoll@gmail.com>
2020-07-19 20:28:52 -0400

    Updated ssh chmod

--- install.sh
+++ install.sh
@@ -33,6 +33,7 @@ echo '. "~/dotfiles/profile.ps1"' >~/.config/powershell/Microsoft.PowerShell_pro
 if [ $WT_SESSION ]; then
     WINDOWS_USER=$(/mnt/c/Windows/System32/cmd.exe /c 'echo %USERNAME%' | sed -e 's/\r//g') > /dev/null 2>&1
     cp -rf /mnt/c/Users/$WINDOWS_USER/.ssh/ ~/.ssh/
+    find ~/.ssh/ -type f -print0  | xargs -0 chmod 644
     pushd ~/dotfiles/
     git remote remove origin
     git remote add origin git@github.com:david-driscoll/dotfiles.git
