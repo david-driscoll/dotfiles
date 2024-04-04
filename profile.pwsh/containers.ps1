3b723ebd9e37b9901240ef7cf1e76f62956265fe
David Driscoll <david.driscoll@gmail.com>
2024-04-04 19:22:29 -0400

    Removed known_hosts from the repo, fixed powershell profiles with ~ paths.

--- profile.pwsh/containers.ps1
+++ profile.pwsh/containers.ps1
@@ -1,6 +1,7 @@
 Set-Alias -Name k -Value kubectl
 Set-Alias -Name d -Value docker
 New-Alias -Name tf -Value terraform.exe
+New-Alias -Name pl -Value pulumi
 
 Import-Module DockerCompletion
 Import-Module PSKubectlCompletion
