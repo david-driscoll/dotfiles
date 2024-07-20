0d8a284b0cec45ef5d685bfe6477099aeb4a6f32
David Driscoll <david.driscoll@gmail.com>
2024-07-20 11:37:06 -0400

    Update brewfile

--- install.sh
+++ install.sh
@@ -19,7 +19,10 @@ fi
 brew bundle --file ./setup/Brewfile
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh | bash -s -- --channel LTS
+wget https://dot.net/v1/dotnet-install.sh \
+    && chmod +x dotnet-install.sh \
+    && ./dotnet-install.sh --channel LTS \
+    && rm dotnet-install.sh
 wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
 
 # volta
