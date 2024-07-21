2609514895f831b313a47276d7460f31bb7755cb
David Driscoll <david.driscoll@gmail.com>
2024-07-21 09:57:29 -0400

    wip on dotfiles

--- install.sh
+++ install.sh
@@ -87,21 +87,22 @@ if [ $WT_SESSION ]; then
     # todo configure for current wsl user
     git config --global gpg."ssh".program "/mnt/c/Program Files/1Password/app/8/op-ssh-sign-wsl"
 elif [[ "$(uname)" != "Darwin" ]]; then
-    sudo -s \
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-    gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-    tee /etc/apt/sources.list.d/1password.list
-    mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-    tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-    mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-    gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-    apt update && apt install 1password-cli
+    ARCH=$(uname -m)
+    case $ARCH in
+        x86_64) ARCH="amd64" ;;
+        armv6l) ARCH="arm" ;;
+        armv7l) ARCH="arm" ;;
+        aarch64) ARCH="arm64" ;;
+        *) echo "Unsupported architecture"; exit 1 ;;
+    esac
+    wget "https://cache.agilebits.com/dist/1P/op2/pkg/v2.29.0/op_linux_${ARCH}_v2.29.0.zip" -O op.zip && \
+        unzip -d op op.zip && \
+        sudo mv op/op /usr/local/bin/ && \
+        rm -rf op.zip op && \
+        sudo groupadd -f onepassword-cli && \
+        sudo chgrp onepassword-cli /usr/local/bin/op && \
+        sudo chmod g+s /usr/local/bin/op
 fi
-
-
 
 git config --global core.eol lf
 git config --global core.autocrlf false
