fb51be55f68e1651903d0fd1709734b1064d3b87
David Driscoll <david.driscoll@gmail.com>
2025-02-15 21:02:07 -0500

    updates

--- profile.ps1
+++ profile.ps1
@@ -5,11 +5,6 @@ if (-not $env:PSModulePath.Contains($DotfilesModulePath) ) {
     $env:PSModulePath = $env:PSModulePath.Insert(0, "$DotfilesModulePath$([System.IO.Path]::PathSeparator)")
 }
 
-write-host $PSScriptRoot
-foreach ($x in Get-ChildItem $PSScriptRoot/profile.pwsh -Filter *.ps1) {
-    . $x.FullName
-}
-
 if ($IsMacOS) {
     . "$PSScriptRoot/profile.darwin.ps1"
 }
@@ -20,17 +15,29 @@ if ($IsLinux) {
     . "$PSScriptRoot/profile.linux.ps1"
 }
 
+foreach ($x in Get-ChildItem $PSScriptRoot/profile.pwsh -Filter *.ps1) {
+    . $x.FullName
+}
+
 $ENV:STARSHIP_CONFIG = Join-Path $PSScriptRoot 'starship.toml';
+$starship = get-command starship;
 
 #Invoke-Expression (&starship init powershell)
-$promptModule = & 'C:\Program Files\starship\bin\starship.exe' init powershell --print-full-init | Out-String;
-$promptModule = $promptModule.Replace("# Return the prompt", @'
-# Return the prompt
+$promptModule = & $starship init powershell --print-full-init | Out-String;
+
+$customPrompt = "";
+if ($IsWindows) {
+    $customPrompt = @'
 $loc = $executionContext.SessionState.Path.CurrentLocation;
 $ext = "$([char]27)]9;12$([char]7)"
 if ($loc.Provider.Name -eq "FileSystem") {
     $ext += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
 }
+
+'@;
+}
+$customPrompt = $customPrompt + @'
+# Return the prompt
 
 $title = $promptText
 $promptText = $ext + $promptText
@@ -38,7 +45,9 @@ $space = $title.IndexOf(' ');
 $gitStop = $title.LastIndexOf(' ');
 $title = $title.Substring($space + 1, ($gitStop - $space)-1)
 $host.UI.RawUI.WindowTitle = ($title -replace '\x1b\[[0-9;]*m', '') -replace '', '📂'
-'@);
+'@;
+
+$promptModule = $promptModule.Replace("# Return the prompt", $customPrompt);
 Invoke-Expression $promptModule
 (volta completions powershell) -join "`n" | Invoke-Expression
 (gh completion -s powershell) -join "`n" | Invoke-Expression
