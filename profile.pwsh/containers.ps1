8c2e396e999f6e57a2a548bb520c7ddfd5cf1fbf
David Driscoll <ddriscoll@relias.com>
2026-04-14 14:22:30 -0400

    updated powershell modules, and a few dotfile behavior updates for new machines

--- profile.pwsh/containers.ps1
+++ profile.pwsh/containers.ps1
@@ -1,8 +1,3 @@
-Set-Alias -Name k -Value kubectl
-Set-Alias -Name d -Value docker
-New-Alias -Name tf -Value terraform.exe
-New-Alias -Name pl -Value pulumi
-
 Import-Module DockerCompletion
 Import-Module PSKubectlCompletion
 Import-Module TerraformCompletion
